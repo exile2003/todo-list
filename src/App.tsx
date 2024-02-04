0053f1856c6c947c604e9a60f3058a722c5fce6c
exile2003 <31631692+exile2003@users.noreply.github.com>
2024-02-04 16:15:51 +0200

    Add component Filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
+import { Filter } from './components/Filter.tsx'
+import { Box, Text, VStack, Divider } from "@chakra-ui/react"
 
-import './App.css'
+
+
+//import './App.css'
 
 function App() {
  // const [count, setCount] = useState(0)
 
   return (
     <>
+    <br />
+    <hr />
+    
+    <VStack mt={8} spacing='8'>
+       <Filter />
        
+       <Divider border='px' borderColor='teal.500' w='100%' />
+       <Filter />
+    </VStack>
     </>
   )
 }
