355d837eb3051ebdb8f0247a932abca7acbe620d
exile2003 <31631692+exile2003@users.noreply.github.com>
2024-02-07 21:47:03 +0200

    Add custom components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { Filter } from './components/Filter.tsx'
 import { Box, Text, VStack, Divider, } from "@chakra-ui/react"
 import Slate from './components/Slate.tsx'
 import Total from './components/Total.tsx'
 import NewTodo from './components/NewTodo.tsx'
 import GetTodos from './components/GetTodos.tsx'
+import ListContext from './context/ListContext.ts'
 
-// import './App.css'
 
 function App() {
- // const [count, setCount] = useState(0)
+  
+  const value = useContext(ListContext)
+
+  const [list, setList] = useState([{
+    id: 0,
+    title: 'Learn JS',
+    todo: false,
+  },
+  {
+    id: 1,
+    title: 'Learn React',
+    todo: false,
+  }])
 
   return (
-    <VStack mt={8} spacing='8' >
+    <ListContext.Provider value={list} >
+       <VStack mt={8} spacing='8' >
        <Filter />
-       {/* <List h={'30px'} /> */}
-       <Slate  style={'300px'} />
+       <Slate />
        <Total />
        <NewTodo />
        <GetTodos />
     </VStack>
+    </ListContext.Provider>
+   
   )
 }
 
