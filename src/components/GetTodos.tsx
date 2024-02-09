838c42fc7d73f79ffbe740b53fa08ca5619830cb
exile2003 <31631692+exile2003@users.noreply.github.com>
2024-02-09 22:05:41 +0200

    Add the interface to the ListContext component

--- src/components/GetTodos.tsx
+++ src/components/GetTodos.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function GetTodos() {
   return (
     <div>GetTodos</div>
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function NewTodo() {
   return (
     <div>NewTodo</div>
