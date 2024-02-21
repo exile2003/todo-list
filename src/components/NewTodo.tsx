ae193f7f18ab45398f3a2a9e278ae2dc30106c98
exile2003 <31631692+exile2003@users.noreply.github.com>
2024-02-21 20:55:53 +0200

    Add offcanvas

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -3,7 +3,7 @@ import {useContext, useState} from 'react'
 import ListContext from '../context/ListContext.ts'
 import {FormControl, Input, Popover, PopoverContent, PopoverBody} from '@chakra-ui/react'
 //import { color } from 'framer-motion';
-import { Offcanvas, Button, Form, InputGroup} from 'react-bootstrap';
+import { Offcanvas, Button, Form} from 'react-bootstrap';
 
     
 function NewTodo() {
@@ -11,7 +11,7 @@ function NewTodo() {
   const value = useContext(ListContext)
 
   const [isFormVisible, setIsFormVisible] = useState(false);
-  const [isEditing, setIsEditing] = useState(true)
+  //const [isEditing, setIsEditing] = useState(true)
   const [data, setData] = useState("")
 
 
@@ -73,7 +73,7 @@ function NewTodo() {
             <br />
             <Form.Control id="inputTitle" type="text" name="title" placeholder="Type here..." onChange={(e) => setData(e.target.value)}/>
             <br /><br />
-            <Button color="white" onClick={saveData} >
+            <Button style={{ backgroundColor: "teal" }} color="white" onClick={saveData} >
               Save
             </Button>
       </Offcanvas.Body>
